bbd95a6460242e1b64ff7c1bcb4dbfb0530c705e
Denis Pettens <denis.pettens@gmail.com>
2016-03-13 09:15:54 +0100

    Create the action of the Sign In button + retrieve the result of the Google API Sign In

--- app/src/main/java/com/ucl/epl/lfsab1509/groupe20/meetinghaters/SignInActivity.java
+++ app/src/main/java/com/ucl/epl/lfsab1509/groupe20/meetinghaters/SignInActivity.java
@@ -1,8 +1,9 @@
 package com.ucl.epl.lfsab1509.groupe20.meetinghaters;
 
-import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
@@ -13,13 +14,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.OptionalPendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 
 public class SignInActivity extends AppCompatActivity implements
-        GoogleApiClient.OnConnectionFailedListener,
-        View.OnClickListener {
+        GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "SignInActivity";
     private static final int RC_SIGN_IN = 9001;
@@ -30,9 +27,6 @@ public class SignInActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
-
-        /* Listeners */
-        findViewById(R.id.sign_in_button).setOnClickListener(this);
 
         /*
          * Google Sign In : https://developers.google.com/identity/sign-in/android/sign-in
@@ -52,26 +46,52 @@ public class SignInActivity extends AppCompatActivity implements
                 .build();
 
         /*
-         * Customization of the Google Sign In Button by setting a wide button
+         * Create a OnClick listener for the Google Sign In Button
+         * and customize it by setting a wide button
          */
-        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_WIDE);
+        SignInButton mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        mSignInButton.setSize(SignInButton.SIZE_WIDE);
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
-        // be available.
+        /*
+         * An unresolvable error has occurred and Google APIs (including Sign-In)
+         * will not be available.
+         */
         Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        /*
+         * Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+         */
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+            /*
+             * Signed in successfully, show authenticated UI.
+             */
+                GoogleSignInAccount account = result.getSignInAccount();
+                String id = account.getId();
+                String name = account.getDisplayName();
+                String mail = account.getEmail();
+                Uri photo = account.getPhotoUrl();
+            }
+        }
     }
 
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onClick(View v) {
-        signIn();
     }
 }
