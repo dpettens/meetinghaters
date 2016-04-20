97dad32e36c05f6e501962aa5ec9cb29e628efb5
Denis Pettens <denis.pettens@gmail.com>
2016-04-20 08:28:29 +0200

    Add the Xml of the signup activity

--- app/src/main/java/com/ucl/epl/lfsab1509/groupe20/meetinghaters/SignUpActivity.java
+++ app/src/main/java/com/ucl/epl/lfsab1509/groupe20/meetinghaters/SignUpActivity.java
@@ -1,8 +1,19 @@
 package com.ucl.epl.lfsab1509.groupe20.meetinghaters;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
     private static final String TAG = "SignUpActivity";
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
 }
