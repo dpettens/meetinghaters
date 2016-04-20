2d916defdde49e416ced5158becfe71e9693ab2a
Denis Pettens <denis.pettens@gmail.com>
2016-04-20 07:36:49 +0200

    Delete old version of google siginin and create signup activty

---
+++ app/src/main/java/com/ucl/epl/lfsab1509/groupe20/meetinghaters/SignUpActivity.java
@@ -0,0 +1,8 @@
+package com.ucl.epl.lfsab1509.groupe20.meetinghaters;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+    private static final String TAG = "SignUpActivity";
+
+}
